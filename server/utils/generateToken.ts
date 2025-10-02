5afb22727fbc7d3dcb89dadddf3c1d2de9f1b77f
Rishi Singh <samarathay123@gmail.com>
2025-10-03 02:03:54 +0530

    update

--- server/utils/generateToken.ts
+++ server/utils/generateToken.ts
@@ -8,7 +8,8 @@ export const generateToken = (res: Response, user: IUserDocument) => {
   });
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
   });
   return token;
